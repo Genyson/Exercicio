823bb5841b67e53c96d4ef312ca86106a87ce7ae
Genyson <genyson_gpb@hotmail.com>
2019-03-04 22:28:01 -0300

    criaçao do pseudocodigo

---
+++ Exercicio2.php
@@ -0,0 +1,29 @@
+<?php
+
+//echo "What do you want to input? ";
+//$input = rtrim(fgets(STDIN));
+//echo "I got it:\n" . $input . "\n";
+
+//Objetivo do programa = Define a Média das Notas da Classe
+//  -insere as notas dos alunos - quantidade indefinida
+//  -calculo a media do total de notas inseridas
+//  -apresento a media das notas
+
+//Declarar as variaveis necessarias
+//  -Quantidade de notas inseridas = 0
+//  -Soma das notas inseridas = 0
+//  -Sentinela = "Calcular"
+
+//Processamento
+//  -insiro a nota do aluno ou sentinela
+//      -se o valor for diferente do sentinela
+//          -somo o valor da nota inserida as demais
+//          -somo a quantidade de notas inseridas
+
+//  se total de notas maior que 0
+//      -calculo a media
+        //Apresento o resultado
+//  se não
+//      apresentar Nenhuma nota inserida
+
+
